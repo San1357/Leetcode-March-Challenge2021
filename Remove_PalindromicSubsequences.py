d3fe48184b56f55a637bae14365d3e4769cf7556
San Ayush <41052723+San1357@users.noreply.github.com>
2021-03-08 19:18:57 +0530

    Create Remove_PalindromicSubsequences.py

---
+++ Remove_PalindromicSubsequences.py
@@ -0,0 +1,9 @@
+'''Problem : Remove Palindromic Subsequences '''
+
+#CODE  :
+
+
+class Solution:
+    def removePalindromeSub(self, S: str) -> int:
+        if not S: return 0
+        return 1 if S == S[::-1] else 2
