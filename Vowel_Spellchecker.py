bada2a09e661aa2be0c8a1fab9853772d6a057d9
San Ayush <41052723+San1357@users.noreply.github.com>
2021-03-23 11:23:04 +0530

    Create Vowel_Spellchecker.py

---
+++ Vowel_Spellchecker.py
@@ -0,0 +1,34 @@
+'''problem : Vowel SpellChecker '''
+
+#CODE :
+class Solution:
+    def spellchecker(self, wordlist, queries):
+        """
+        :type wordlist: List[str]
+        :type queries: List[str]
+        :rtype: List[str]
+        """
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        def todev(word):
+            return "".join('*' if c.lower() in vowels else c.lower()
+                           for c in word)
+
+        words = set(wordlist)
+        caps = {}
+        vows = {}
+
+        for word in wordlist:
+            caps.setdefault(word.lower(), word)
+            vows.setdefault(todev(word), word)
+
+        def check(query):
+            if query in words:
+                return query
+            lower = query.lower()
+            if lower in caps:
+                return caps[lower]
+            devow = todev(lower)
+            if devow in vows:
+                return vows[devow]
+            return ""
+        return map(check, queries)
